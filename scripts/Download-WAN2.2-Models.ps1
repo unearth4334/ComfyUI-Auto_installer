b97dde9f78973f7af72c235cc0baf27960b04edb
PC-FIXE <contact@smoreno.me>
2025-08-23 16:31:21 +0800

    linkfix

--- scripts/Download-WAN2.2-Models.ps1
+++ scripts/Download-WAN2.2-Models.ps1
@@ -120,7 +120,7 @@ if ($T2VChoice -ne 'G') {
     }
     if ($T2VChoice -in 'B', 'F') {
         Download-File -Uri "$baseUrl/diffusion_models/WAN/wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors" -OutFile (Join-Path $wanDiffDir "wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors")
-        Download-File -Uri "$baseUrl/diffusion_models/WAN/wan2.2_t2v_low_14B_fp8_scaled.safetensors" -OutFile (Join-Path $wanDiffDir "wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors")
+        Download-File -Uri "$baseUrl/diffusion_models/WAN/wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors" -OutFile (Join-Path $wanDiffDir "wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors")
     }
     if ($T2VChoice -in 'C', 'F') {
         Download-File -Uri "$baseUrl/unet/WAN/Wan2.2-T2V-A14B-HighNoise-Q8_0.gguf" -OutFile (Join-Path $wanUnetDir "Wan2.2-T2V-A14B-HighNoise-Q8_0.gguf")
